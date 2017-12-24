6a614c79357cb1aab63846714db2bf1cf84a9922
Jacob Leach <leach.jacob@gmail.com>
2017-12-24 14:40:47 -0500

    Add quick sidebar that doesn't do anything yet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" class="full-height">
+    <div class="left-bar">
+      <div class="left-button">
+        Videos
+      </div>
+      <div class="left-button">
+        Libraries
+      </div>
+    </div>
+    <div class="content-pane">
+      <router-view>
+      </router-view>
+    </div>
   </div>
 </template>
 
@@ -11,10 +22,40 @@ export default {
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+
 body {
+  height: 100%;
   margin: 0px;
   background-color: #151515;
   color: #DDDDDD;
+}
+
+.full-height {
+  height: 100%;
+}
+
+.left-bar {
+  background-color: #222222;
+  float: left;
+  height: 100%;
+  width: 100px;
+}
+
+.left-button {
+  padding-left: 10px;
+  padding-top: 7px;
+  padding-bottom: 7px;
+}
+
+.left-button:hover {
+  background-color: #404040;
+}
+
+.content-pane {
+  margin-left: 100px;
 }
 
 a {
